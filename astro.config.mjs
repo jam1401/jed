fcead861b655ca154afd9e3c532f852433fa478e
Jeff Morgan <jam1401@gmail.com>
2024-11-21 16:04:54 -0800

    tailwind removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+
 
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sanity({
+  integrations: [ sanity({
     projectId: 'sz2nnsm0',
     dataset: 'production',
     useCdn: false, // See note on using the CDN
