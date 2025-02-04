f88aeb06959420ea6120c43297228f27616fe3ab
Jeff <jam1401@gmail.com>
2025-02-04 13:31:43 +0000

    removed sanity temporarily

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-
-
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ sanity({
+  integrations: [],
+});
+
+/**
+ * 
+ * sanity({
     projectId: 'sz2nnsm0',
     dataset: 'production',
     useCdn: false, // See note on using the CDN
-    apiVersion: "2024-07-24", // insert the current date to access the latest version of the API
-  }), react()]
-});
+    apiVersion: "2024-07-24", ]// insert the current date to access the latest version of the API
+  }), react()
+ */
